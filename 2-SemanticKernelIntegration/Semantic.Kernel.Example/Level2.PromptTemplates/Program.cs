b05b354cd226ed22f60512086227fd4ebe07e50f
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-05-13 20:49:47 +0300

    feat: Added usage of prompt templates in Semantic Kernel

---
+++ 2-SemanticKernelIntegration/Semantic.Kernel.Example/Level2.PromptTemplates/Program.cs
@@ -0,0 +1,18 @@
+﻿using Codeblaze.SemanticKernel.Connectors.Ollama;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+
+IKernelBuilder? builder = Kernel.CreateBuilder()
+    .AddOllamaChatCompletion("deepseek-r1:latest", "http://localhost:11434");
+
+builder.Services.AddScoped<HttpClient>();
+
+Kernel? kernel = builder.Build();
+
+string? promptTemplate = "Yandaki işlemi hesapla: {{$input}}";
+KernelFunction? function = kernel.CreateFunctionFromPrompt(promptTemplate);
+KernelArguments? arguments = new KernelArguments { ["input"] = "1 + 2 + 5 * 3" };
+FunctionResult? result = await function.InvokeAsync(kernel, arguments);
+Console.WriteLine(result);
+
+Console.Read();
