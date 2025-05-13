bc89387b4b28e9af635d576a078f5739de2604d3
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-05-13 20:43:32 +0300

    feat: Added Semantic Kernel basics to .NET application

---
+++ 2-SemanticKernelIntegration/Semantic.Kernel.Example/Level1.Essentials/Program.cs
@@ -0,0 +1,18 @@
+﻿using Codeblaze.SemanticKernel.Connectors.Ollama;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+
+IKernelBuilder? builder = Kernel.CreateBuilder()
+    .AddOllamaChatCompletion("deepseek-r1:latest", "http://localhost:11434");
+
+builder.Services.AddScoped<HttpClient>();
+
+Kernel? kernel = builder.Build();
+
+while (true)
+{
+    Console.WriteLine("Soru Sor : ");
+    string input = Console.ReadLine();
+    FunctionResult? response = await kernel.InvokePromptAsync(input);
+    Console.WriteLine($"Cevap : \n------------------------------------\n{response}\n------------------------------------");
+}
